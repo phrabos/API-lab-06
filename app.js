753f665d5ddff9810dc279fa93a4a330da789917
patrick hrabos <phrabos@gmail.com>
2021-02-18 20:51:46 -0500

    remove console log

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ app.get('/tea/', (req, res) => {
 app.get('/tea/:id', (req, res) => {
     const id = Number(req.params.id);
     const selectedTea = data.find((tea) => tea.id === id);
-    console.log(selectedTea)
-
     res.json({
         selectedTea
       })
