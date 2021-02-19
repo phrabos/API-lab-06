6a0734bc23f521d320113b24afcf240aa8961782
patrick hrabos <phrabos@gmail.com>
2021-02-18 20:21:33 -0500

    add a category endpoint

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -20,3 +20,30 @@ it('should return a single tea objects', async (done) => {
     expect(response.body).toEqual(expectation)
     done()
   })
+  it('should return a single tea objects', async (done) => {
+    const expectation =  [  
+    {
+        id: 1, 
+        name: 'Da Hong Pao',
+        image: 'dahongpao.jpg',
+        description: 'roasted rock tea from Wuyi Mountain',
+        category: 'Yancha',
+        price: 35,
+        aged: false,
+    },
+    {
+        id: 5, 
+        name: 'Rou Gui',
+        image: 'rougui.jpg',
+        description: 'roated spicy cinnamon bark flavor',
+        category: 'Yancha',
+        price: 25,
+        aged: false,
+    },
+]
+    const response = await request.get('/tea/category/Yancha')
+  
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(expectation)
+    done()
+  })
--- app.js
+++ app.js
@@ -21,9 +21,17 @@ app.get('/tea/', (req, res) => {
 app.get('/tea/:id', (req, res) => {
     const id = Number(req.params.id);
     const selectedTea = data.find((tea) => tea.id === id);
+    console.log(selectedTea)
 
     res.json({
         selectedTea
+      })
+  })
+  app.get('/tea/category/:category', (req, res) => {
+    const category = req.params.category;
+    const teaByCategory = data.filter((tea) => tea.category === category);
+    res.json({
+        teaByCategory
       })
   })
 
